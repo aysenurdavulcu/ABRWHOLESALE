c65f7b82c490cf441149f48254c091a9297610cf
Ayse Dav <aysenurdavulcu56@gmail.com>
2023-07-22 14:48:13 -0400

    all files added

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+import logo from './logo.svg';
+import './App.css';
+import { SearchBar } from './components/SearchBar';
+import { Login} from "./components/Login";
+import { Register} from "./components/Register";
+
+
+
+function App()  {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
+  return (
+    <div className="App">
+      <div className="search-bar-container">
+        {/* <div className = "title">ABR Wholesale </div> */}
+        {/* <SearchBar/> */}
+        {/* { 
+          currentForm === "login" ? <Login onFormSwitch = {toggleForm} /> : <Register />
+        } */}
+
+        {/* <div className = "new-items">Browse our newest items</div> */}
+        {/* <div>SearchResults</div> */}
+        {/* <Products/> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React, {useDeferredValue, useState} from 'react'
+import {FaSearch} from "react-icons/fa";
+import "./SearchBar.css";
+
+export const SearchBar = () => {
+    const [input, setInput] = useState("");
+
+    const fetchData = (value) => {
+        fetch("https://jsonplaceholder.typicode.com/users").then((response) => response.json()).then(json =>{
+           const results = json.filter((user) => {
+               return user && user.name && user.name.toLowerCase().includes(useDeferredValue)
+           });
+           console.log(results);
+        });
+    }
+
+    const handleChange = (value)=>{
+        setInput(value);
+        fetchData(value);
+    };
+
+return (
+<div className='input-wrapper'>
+    <FaSearch id = "search-icon"/>
+    <input placeholder = "Type to search..." 
+    value= {input} 
+    onChange={(e) => handleChange(e.target.value)}/>
+</div>
+);
+};
